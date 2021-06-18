bbbeb2438b0ff064ea74320cc084d4246c655e30
Roeland Macabenta Rotairo <85470341+Rotairo@users.noreply.github.com>
2021-06-19 00:57:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>COMPILATION OF ALL ACTIVITIES</title>
+</head>
+
+<body>
+<div id="container">
+        <div id="header">
+            <h1>IPT-<span class="off">1</span></h1>
+            <h2>Rotairo,Roeland M. / BSIT-2C</h2>
+        </div>   
+        
+        <div id="menu">
+            <ul>
+                <li class="menuitem"><a href="#">Home</a></li>
+            </ul>
+        </div>
+        
+        <div id="leftmenu">
+
+        <div id="leftmenu_top"></div>
+
+                <div id="leftmenu_main">    
+                
+                <h3>Links</h3>
+                        
+                <ul>
+                    <li><a href="https://drive.google.com/file/d/1k0tzwdxrLHCGZiPFx02bMhhFH6EZfr2n/view?usp=sharing">Scripting Language and Programming Language</a></li>
+                    <li><a href="https://drive.google.com/file/d/1g58zgtp2NfoDz-dmCWgwax-k6KzuRdle/view?usp=sharing">What is PHP?</a></li>
+                    <li><a href="https://drive.google.com/file/d/1gzS1unFTJ3b3a6rlrpr-ME2duJvgoXbU/view?usp=sharing">What is Xampp?</a></li>
+                    <li><a href="https://drive.google.com/file/d/1ryqVaO5MpX0Py-cuwf8FKk5_SWL6i6og/view?usp=sharing">PHP,ASP.NET,JSP and CFML</a></li>
+                    <li><a href="https://drive.google.com/file/d/1a7j0nbs98t8wgITJdMH_KMdEoSvKUM1-/view?usp=sharing">Types of Data Types</a></li>
+                    <li><a href="https://drive.google.com/file/d/1hVE9Db7kqHzszSe4zC9pTOXMlgwtnOtx/view?usp=sharing">PHP Variable and Constant Variable</a></li>
+                    <li><a href="https://drive.google.com/file/d/19rGpZCfhMZ4d4s7hxlfL6qVTYQGv7hIX/view?usp=sharing">PHP Operators</a></li>
+                    <li><a href="https://drive.google.com/file/d/17bMMpW5XCOcgqIIjCuUh52hAPrbvrWrV/view?usp=sharing">Control Structures</a></li>
+                    <li><a href="https://drive.google.com/file/d/1okYd4OoJQmx_CWDBk-RTqbIzHaJTN2nl/view?usp=sharing">Github and Netlify Account</a></li>
+               
+                </ul>
+</div>
+                
+                
+              <div id="leftmenu_bottom"></div>
+        </div>  
+		<div id="content">
+        
+        
+        <div id="content_top"></div>
+        <div id="content_main">
+        
+        	<h1>FINAL PROJECT IN </h1>
+        	<h1>"INTERACTIVE PROGRAMMING TECHNOLOGIES-1"</h1>
+        </div>
+      <div id="content_bottom"></div>
+            
+            <div id="footer"><h3><a href="http://www.bryantsmith.com">All Rights Reserved 2021!</a></h3></div>
+      </div>
+   </div>
+</body>
+</html>
